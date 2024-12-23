05c0e4abe9505589c09874512847f18c2d3ee394
paris26 <paris.gvr@gmail.com>
2024-12-23 12:33:10 +0200

    Finishes Sanity Schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { startup} from "@/sanity/schemaTypes/startup";
+import { author } from "@/sanity/schemaTypes/author";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [author, startup],
 }
--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,7 +1,13 @@
 import type {StructureResolver} from 'sanity/structure'
+import { author} from "@/sanity/schemaTypes/author";
+import { startup } from "@/sanity/schemaTypes/startup";
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
     .title('Content')
-    .items(S.documentTypeListItems())
+    .items([
+        S.documentTypeListItem("author").title("Authors"),
+        S.documentTypeListItem("startup").title("Startups")
+
+    ])
